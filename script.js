a32ea6ebc908b2fd33ee300e48646a3e486622dc
S. Muthu Krishnan <1907muthukrishnan@gmail.com>
2025-01-11 20:13:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var a = document.getElementById("tbody");
+function showCustomAlert(title, message) {
+    document.getElementById('customAlertTitle').innerText = title;
+    document.getElementById('customAlertMessage').innerText = message;
+    document.getElementById('customAlert').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  
+  function closeCustomAlert() {
+    document.getElementById('customAlert').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
+
+  
+  
+
+
+function deleteMe(event) {
+    event.target.parentElement.parentElement.remove();
+}
+
+function addData() {
+    if (document.getElementById("name").value && document.getElementById("age").value &&  document.getElementById("course").value && document.getElementById("email").value !== "") {
+        let nam = document.getElementById("name").value;
+        let age = document.getElementById("age").value;
+        let male = document.querySelector('input[name="option"]:checked');
+        let female = male.value;
+
+        let course = document.getElementById("course").value;
+        let email = document.getElementById("email").value;
+        a.innerHTML += `<tr><td>${nam}</td><td>${age}</td><td>${course}</td><td>${female}</td><td>${email}</td><td><button  class="dlt" onclick="deleteMe(event)" >delete</button></td></tr>`;
+
+    } else {
+        showCustomAlert('Warning !', 'Fill the Form before Add!!!');
+    }
+}
+
